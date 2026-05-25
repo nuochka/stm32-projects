01cd31326d4dd2c038a805f8d5c8a2847db7aa81
nuochka <anstskovalenko@gmail.com>
2026-05-25 15:56:52 +0200

    Implement TFT display driver and add color bar test patterns

---
+++ 07_SPI_TFT/Core/Inc/lcd.h
@@ -0,0 +1,45 @@
+/*
+ * lcd.h
+ *
+ *  Created on: May 25, 2026
+ *      Author: anastasiiakovalenko
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include "main.h"
+
+#define ST7735S_SLPOUT			0x11
+#define ST7735S_DISPOFF			0x28
+#define ST7735S_DISPON			0x29
+#define ST7735S_CASET			0x2a
+#define ST7735S_RASET			0x2b
+#define ST7735S_RAMWR			0x2c
+#define ST7735S_MADCTL			0x36
+#define ST7735S_COLMOD			0x3a
+#define ST7735S_FRMCTR1			0xb1
+#define ST7735S_FRMCTR2			0xb2
+#define ST7735S_FRMCTR3			0xb3
+#define ST7735S_INVCTR			0xb4
+#define ST7735S_PWCTR1			0xc0
+#define ST7735S_PWCTR2			0xc1
+#define ST7735S_PWCTR3			0xc2
+#define ST7735S_PWCTR4			0xc3
+#define ST7735S_PWCTR5			0xc4
+#define ST7735S_VMCTR1			0xc5
+#define ST7735S_GAMCTRP1		0xe0
+#define ST7735S_GAMCTRN1		0xe1
+
+#define BLACK     0x0000
+#define RED       0xf800
+#define GREEN     0x07e0
+#define BLUE      0x001f
+#define YELLOW    0xffe0
+#define MAGENTA   0xf81f
+#define CYAN      0x07ff
+#define WHITE     0xffff
+
+void lcd_init(void);
+
+void lcd_fill_box(int x, int y, int width, int height, uint16_t color);
