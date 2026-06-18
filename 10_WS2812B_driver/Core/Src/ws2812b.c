cd614c3d5f78a60fb9ee2979da6e012cf4dc35fb
nuochka <anstskovalenko@gmail.com>
2026-06-18 18:28:47 +0200

    Implement WS2812B LED driver via TIM+DMA

---
+++ 10_WS2812B_driver/Core/Src/ws2812b.c
@@ -0,0 +1,53 @@
+#include "ws2818b.h"
+#include "tim.h"
+
+#define BIT_0_TIME	32
+#define BIT_1_TIME	64
+
+#define RESET_LEN	40
+#define LED_N		7
+#define TOTAL_LEN   (RESET_LEN + 24 * LED_N + 1)
+
+
+static uint16_t led_buffer[TOTAL_LEN * 2];
+
+static void set_byte(uint32_t pos, uint8_t value){
+	for(int i = 0; i < 8; i++){
+		if(value & 0x80){
+			led_buffer[pos + i] = BIT_1_TIME;
+		} else {
+			led_buffer[pos + i] = BIT_0_TIME;
+		}
+		value <<= 1;
+	}
+}
+
+void ws2812b_init(void){
+	for(int i = 0; i < TOTAL_LEN * 2; i++){
+		led_buffer[i] = 0;
+	}
+
+	for(int i = 0; i < 24 * LED_N; i++){
+		led_buffer[RESET_LEN + i] = BIT_0_TIME;
+	}
+
+	led_buffer[TOTAL_LEN - 1] = 100;
+
+	ws2812b_update();
+}
+
+void ws2812b_update(void){
+	HAL_TIM_PWM_Stop_DMA(&htim3, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_1, (uint32_t *)led_buffer, TOTAL_LEN * 2);
+}
+
+void ws2812b_wait(void){
+}
+
+void ws2812b_set_color(uint32_t led, uint8_t red, uint8_t green, uint8_t blue){
+	if(led < LED_N){
+		set_byte(RESET_LEN + 24 * led, green);
+		set_byte(RESET_LEN + 24 * led + 8, red);
+		set_byte(RESET_LEN + 24 * led + 16, blue);
+	}
+}
